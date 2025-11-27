192e1e2e0de9fd88ad32cbdc70ee448c3c604084
brian-p-tran <briantran.bpt@gmail.com>
2025-11-27 13:21:33 -0800

    CustomerDAO Methods Added

--- backend/src/main/java/com/project/cs157a/CustomerDAO.java
+++ backend/src/main/java/com/project/cs157a/CustomerDAO.java
@@ -1,5 +1,36 @@
 package com.project.cs157a;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CustomerDAO {
-    
+    public void addCustomer(String name, String email, String phone) throws SQLException {
+        String sql = "INSERT INTO Customers (CustomerName, Email, Phone) VALUES (?, ?, ?)";
+
+        Connection con = DBCon.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ps.setString(1, name);
+        ps.setString(2, email);
+        ps.setString(3, phone);
+        ps.executeUpdate();
+
+        con.close();
+        
+    }
+
+    public void viewCustomers() throws SQLException {
+        String sql = "SELECT * FROM Customers";
+
+        Connection con = DBCon.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ResultSet res = ps.executeQuery();
+
+        //res contains the set of the entries, use res.getInt or res.getString
+
+        con.close();
+    }
 }
